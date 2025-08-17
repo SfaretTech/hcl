b0ddf56a7ba730180921264dcf701f36d5dc4d53
HCOM <healthcommunityintl@gmail.com>
2025-08-17 23:31:08 +0000

    replace it with the icon logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,17 +2,19 @@
 import * as React from 'react';
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2ZM8 13.5V11h8v2.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5ZM8 10.5V8.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v2H8Zm5 0V8.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v2h-3Z"
-      fill="currentColor"
-    />
-  </svg>
+    <svg 
+        viewBox="0 0 100 100" 
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <circle cx="50" cy="50" r="48" fill="hsl(var(--primary))" stroke="hsl(var(--primary-foreground))" strokeWidth="2" />
+        <path 
+            d="M 30 70 L 30 30 L 40 30 C 60 30 60 50 40 50 L 70 50 M 70 30 L 70 70" 
+            stroke="hsl(var(--primary-foreground))" 
+            strokeWidth="5"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
 );
